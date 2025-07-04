97d3ff2c4c6110d4a70a631ef091c91a281ee690
Luka Khanjaladze <lkhanjaladze@gmail.com>
2025-07-04 23:49:54 +0400

    added WAR for deployment

--- TaskManager/src/main/java/com/example/TaskManager/TaskManagerApplication.java
+++ TaskManager/src/main/java/com/example/TaskManager/TaskManagerApplication.java
@@ -2,11 +2,19 @@ package com.example.TaskManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
 @EnableAsync
-public class TaskManagerApplication {
+public class TaskManagerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TaskManagerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagerApplication.class, args);
